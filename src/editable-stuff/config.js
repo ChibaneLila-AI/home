21ca57b3b3d8a583abee82070b151f5f3d6ee308
Chibane Lila <91986238+ChibaneLilaAI@users.noreply.github.com>
2023-12-14 11:58:54 +0100

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -15,22 +15,13 @@ const mainBody = {
       image: "fa-github",
       url: "https://github.com/hashirshoaeb",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+  
+  
     {
       image: "fa-linkedin",
       url: "https://www.linkedin.com/in/hashirshoaeb/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+   
   ],
 };
 
